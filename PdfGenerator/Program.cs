fc524213576183edb67abad58acebdf391e37b73
Maytham Fahmi <mf@comcare.dk>
2018-07-23 09:42:05 +0200

    Init working program is done

--- PdfGenerator/Program.cs
+++ PdfGenerator/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SelectPdf;
+using System;
+using System.IO;
 
 namespace PdfGenerator
 {
@@ -6,7 +8,45 @@ namespace PdfGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string invoice = CreateInvoice();
+            string saveTo = @"c:\temp\invoice.pdf";
+
+            if (invoice != null)
+            {
+                Console.WriteLine("Done");
+                HtmlToPdf(invoice, saveTo);
+            }
         }
+
+        private static string CreateInvoice()
+        {
+            var root = AppDomain.CurrentDomain.BaseDirectory;
+
+            var template = $"{root}\\template\\invoice.html";
+
+            if (!File.Exists(template))
+            {
+                return null;
+            }
+
+            return File.ReadAllText(template);
+        }
+
+        private static void HtmlToPdf(string htmlDoc, string outputFolder)
+        {
+            // instantiate a html to pdf converter object
+            HtmlToPdf converter = new HtmlToPdf();
+
+            // create a new pdf document converting an url
+            PdfDocument doc = converter.ConvertHtmlString(htmlDoc);
+
+            // save pdf document
+            doc.Save(outputFolder);
+
+            // close pdf document
+            doc.Close();
+        }
+
     }
 }
+
